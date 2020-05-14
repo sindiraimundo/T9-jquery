5e2ffe2508a70d77ddae41f1355e1a60710e432e
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-05-14 11:18:43 -0300

    explicacao aula 3 exemplo de scroll down e scroll up

--- Aula 3 - Animações/Exemplos/02_scroll/2_scrolldown/js/script.js
+++ Aula 3 - Animações/Exemplos/02_scroll/2_scrolldown/js/script.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
     console.log($("#button").offset(), 'quem é o offset')
+   console.log($("#button").position(), 'position left')
 
-    $("#button").click(function(){
-        alert("Left:" + $(this).offset().left)// para chamar o objeto colocamos .offset(). a propriedade do objeto que vc que chamar
-    })
+    // $("#button").click(function(){
+    //     alert("Left:" + $(this).offset().left)// para chamar o objeto colocamos .offset(). a propriedade do objeto que vc que chamar
+    // })
 
     //offset é uma função que retorna um objeto
+
+    //"descer" trabalhar com ==> referencia
+    $(".down").click(function(){
+        $("html, body").animate({
+            scrollTop: $(".up").offset().top,
+        },
+        5000
+        );
+    });
+
+    // "subir" ==> retornando as origens
+    $(".up").click(function(){
+        $("html, body").animate({
+            scrollTop: "0"
+        },    
+        5000
+        );
+    })
 
 })
