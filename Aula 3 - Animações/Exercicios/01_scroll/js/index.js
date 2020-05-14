dcbd4c3e7adbe415b660418a9e66d1375c3ac0bf
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-05-14 17:11:47 -0300

    aula 3 resolucao exercicio final scroll

--- Aula 3 - Animações/Exercicios/01_scroll/js/index.js
+++ Aula 3 - Animações/Exercicios/01_scroll/js/index.js
@@ -5,12 +5,14 @@
 //     o scroll acontecerá de forma lenta, direcionado o scroll para o heading correspondente 
 //         ex. se eu clicar no link 3, o scroll deve me direcionar para o heading 3
         $(document).ready(function(){
+
+                     //MENU ELEVADOR
             //linkando link1 e fazendo scroll
             $(".link1").click(function(){
                $("html, body").animate({
                    scrollTop: $("#h1").offset().top,
                },
-               4000
+               1000
                );
             });
 
@@ -19,7 +21,7 @@
                 $("html, body").animate({
                     scrollTop: $("#h2").offset().top,
                 },
-                4000
+                1000
                 );
              });
 
@@ -28,15 +30,33 @@
                 $("html, body").animate({
                     scrollTop: $("#h3").offset().top,
                 },
-                4000
+                1000
                 );
              });
 
+            //     2. Criar o botão de scroll até o topo da página. Lembrando que esse botão só deve aparecer 
+            //     a partir do momento que o scroll começou a ir para baixo. 
 
-//     2. Criar o botão de scroll até o topo da página. Lembrando que esse botão só deve aparecer 
-//     a partir do momento que o scroll começou a ir para baixo. 
+                    /*****************
+                      SCROLL TOP NO BOTÃO TOPS
+                         *****************/
+        $(document).scroll(function(){
 
+            let topPosition = $(this).scrollTop()
 
-// */
-
-        })
+            if(topPosition > 100){
+                $(".scrollTop").css("opacity", "1")
+                //scroll apareça
+            } else {
+                $(".scrollTop").css("opacity", "0")
+                //scroll fica escondido ==> display none
+            }
+            $(".scrollTop").click(function() {
+                $("html,body").animate({
+                    scrollTop: 0,
+                },
+                1000
+                );
+            });
+        });
+    })
